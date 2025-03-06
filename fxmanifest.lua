5d02bc6fe0ae5771c80513eb65bb52e498f1e79f
Gurpeeet-singh <gurpreetsingh.19842008@gmail.com>
2025-03-06 11:35:49 +0000

    created an fx manifest file

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cereluan'
+game 'gta5'
+
+author 'GMAN'
+description 'PD and EMS checker'
+version '1.0'
+
+
+shared_script 'config.lua'
+server_script 'server.lua'
+client_script 'client.lua'
+ui_page 'html/index.html'
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
